711a0456441489e11929e72cb2444fb90cf14e20
Ricardo <ricardodiasmorais@gmail.com>
2024-08-19 11:30:51 +0100

    Under construction message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import HatLogo from './assets/HatLogo';
+
 function App() {
-  return <></>;
+  return (
+    <main
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh',
+        flexDirection: 'column',
+        gap: '30px',
+        padding: '30px',
+      }}
+    >
+      <HatLogo height={200} />
+      <span style={{ letterSpacing: 1.5, textAlign: 'center' }}>
+        🚧This website is currently under development. Stay tuned for updates!
+        Explore the project’s code on{' '}
+        <a href="https://github.com/RicardoMoraisPOR/HexAlphaTool/tree/dev">
+          GitHub
+        </a>
+        .🚧
+      </span>
+    </main>
+  );
 }
 
 export default App;
